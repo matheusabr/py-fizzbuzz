13e4c416aa2a2f9e0dccb2cb6827e67dac4beb2b
Matheus Abreu <matheusabreu.dev@gmail.com>
2017-10-30 17:38:45 -0300

    chore: update tests

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -24,24 +24,35 @@ def robot(pos):
 
     return say
 
+## TESTS
+def assert_equal(result, expected):
+    from sys import _getframe
+
+    msg = 'FAIL: Line {} got: {} expecting: {}'
+
+    if not result == expected:
+        current = _getframe()
+        caller = current.f_back
+        line_no = caller.f_lineno
+        print(msg.format(line_no, result, expected))
 
 if __name__ == "__main__":
     # Case 4
-    assert robot(1) == '1'
-    assert robot(2) == '2'
-    assert robot(4) == '4'
+    assert_equal(robot(1), '1')
+    assert_equal(robot(2), '2')
+    assert_equal(robot(4), '4')
 
     # Case 1
-    assert robot(3) == 'fizz'
-    assert robot(6) == 'fizz'
-    assert robot(9) == 'fizz'
+    assert_equal(robot(3), 'fizz')
+    assert_equal(robot(6), 'fizz')
+    assert_equal(robot(9), 'fizz')
 
     # Case 2
-    assert robot(5) == 'buzz'
-    assert robot(10) == 'buzz'
-    assert robot(20) == 'buzz'
+    assert_equal(robot(5), 'buzz')
+    assert_equal(robot(10), 'buzz')
+    assert_equal(robot(20), 'buzz')
 
     # Case 3
-    assert robot(15) == 'fizzbuzz'
-    assert robot(30) == 'fizzbuzz'
-    assert robot(45) == 'fizzbuzz'
+    assert_equal(robot(15), 'fizzbuzz')
+    assert_equal(robot(30), 'fizzbuzz')
+    assert_equal(robot(45), 'fizzbuzz')
